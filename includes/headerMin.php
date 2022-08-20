50a80b8df13c20361405fe4d2a743806314a9029
Ted Miller <ted.miller92@gmail.com>
2022-08-19 21:20:51 -0700

    headerMin and started whipser case study page

---
+++ includes/headerMin.php
@@ -0,0 +1,47 @@
+<!--BEGIN INCLUDE HEADER-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ScripTedMiller</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Ted Miller Web Developer">
+        
+        <!-- <link rel="stylesheet" href="css/styles.css"> -->
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+
+        <!--Google Font "Roboto Mono"-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+        
+        <!--FontAwesome icons-->
+        <script src="https://use.fontawesome.com/e90390b66f.js"></script>
+        <script src="https://kit.fontawesome.com/6c543eb094.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid col-md-8">
+                <a class="navbar-brand" href="index.php">ScripTedMiller</a>
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse" id="navbarColor01" style="">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./work.php">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./bio.php">Bio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./resume.php">Resume</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./contact.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
--- whisperWheelWorks.php
+++ whisperWheelWorks.php
@@ -1,5 +1,39 @@
-<?php include "./includes/header.php" ?>
+<?php include "./includes/headerMin.php" ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 my-4">
+            <h3 class="display-3">Whisper Wheel Works</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <p>
+                This is a website built for a local business owner.
+            </p>
+            <p>
+                He wanted a simple website that would give a background of his business, 
+                a description of services available, a list of dealers, brands and vendors, 
+                and a simple contact form. 
+                Having an online presence meant he could help prove 
+                his legitimacy as a business to vendors and customers. 
+            </p>
+            <p>
+                I set up a document that we could each contribute to, 
+                where I shared information about hosting and the logistics of creating a domain name. 
+                I also prompted him to provide all the information he wanted on the website, 
+                and we worked back and forth to divide and compile this information 
+                into logical sections that eventually turned into the pages of the website.
+            </p>
+        </div>
+        <div class="col-lg-6">
+            <img class="img-fluid" src="images/whisperWheelWorks-desktopView.png" alt="whisperWheelWorks-desktopView.png">
+        </div>
+    </div>
+    <div class="row">
+        
+    </div>
+</div>
 
 
 <?php include "./includes/footer.php" ?>
